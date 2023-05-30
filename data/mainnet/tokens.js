0ccfc58567db86738d141828d7717edfbde4f6f6
CryptJS13 <jasper@slimmens.nl>
2023-05-30 18:52:51 +0200

    Add fxgrail icon

--- data/mainnet/tokens.js
+++ data/mainnet/tokens.js
@@ -121,7 +121,7 @@ module.exports = {
   },
   fxGRAIL: {
     chain: CHAINS_ID.ARBITRUM_ONE,
-    logoUrl: [''],
+    logoUrl: ['./icons/xgrail.svg'],
     tokenAddress: addresses.ARBITRUM_ONE.V2.xGRAIL.NewVault,
     decimals: '18',
     vaultAddress: null,
