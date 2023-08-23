4e2dcda3711b049d5b394c75a7ee0e90632967bc
CryptJS13 <jasper@slimmens.nl>
2023-08-23 14:02:37 +0200

    Update baseswap

---
+++ src/prices/implementations/baseswap-pair.js
@@ -0,0 +1,39 @@
+const BigNumber = require('bignumber.js')
+const { web3BASE } = require('../../lib/web3')
+
+const uniswapContract = require('../../lib/web3/contracts/uniswap/contract.json')
+const uniswapMethods = require('../../lib/web3/contracts/uniswap/methods')
+
+const addresses = require('../../../data/mainnet/addresses.json')
+
+const getPrice = async (
+  inTokenAddress,
+  outTokenAddress = addresses.BASE.USDC,
+  outTokenDecimals = 6,
+) => {
+  const selectedAddresses = [inTokenAddress, outTokenAddress]
+  const isOutTokenWETH = outTokenAddress === addresses.BASE.WETH
+  if (isOutTokenWETH) {
+    selectedAddresses.push(addresses.BASE.USDC)
+  }
+
+  const uniswapInstance = new web3BASE.eth.Contract(
+    uniswapContract.abi,
+    '0x327Df1E6de05895d2ab08513aaDD9313Fe505d86',
+  )
+
+  const result = await uniswapMethods.getAmountsOut(
+    new BigNumber(10).pow(18).toString(),
+    selectedAddresses,
+    uniswapInstance,
+  )
+
+  const price = new BigNumber(result[isOutTokenWETH ? 2 : 1]).dividedBy(
+    new BigNumber(10).pow(outTokenDecimals),
+  )
+  return price.toString()
+}
+
+module.exports = {
+  getPrice,
+}
