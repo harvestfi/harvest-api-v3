ecfdb3edcac5a2cefd74e4522f612647ab452832
William <williamliu9426@gmail.com>
2024-09-16 03:21:49 -0400

    fix: update camelot api url

--- src/lib/third-party/camelot.js
+++ src/lib/third-party/camelot.js
@@ -4,7 +4,7 @@ const { cachedAxios } = require('../db/models/cache')
 const getCamelotData = isV3 => {
   let uri = isV3
     ? 'https://api.camelot.exchange/v2/liquidity-v3-data/'
-    : 'https://api.camelot.exchange/pools-v2/'
+    : 'https://api.camelot.exchange/v2/liquidity-v2-pools-data/'
   let res = cachedAxios
     .get(uri)
     .then(response => get(response, 'data'))
