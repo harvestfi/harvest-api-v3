1dd6c936729de4bffd7cc8bebbf364bb1f5b5bc2
William <williamliu9426@gmail.com>
2024-09-19 16:16:10 -0400

    feat: add arbitrum notional vaults

--- src/prices/implementations/notional.js
+++ src/prices/implementations/notional.js
@@ -1,15 +1,21 @@
 const BigNumber = require('bignumber.js')
-const { web3 } = require('../../lib/web3')
+const { getWeb3 } = require('../../lib/web3')
 const { abi: notionalContractAbi } = require('../../lib/web3/contracts/notional/contract.json')
 const {
   getNTokenPresentValueUnderlyingDenominated,
 } = require('../../lib/web3/contracts/notional/methods')
 const { getTokenPrice } = require('../index')
 const { token: tokenContractData } = require('../../lib/web3/contracts')
+const { CHAIN_IDS } = require('../../../data/constants')
 
-const notionalProxy = '0x6e7058c91F85E0F6db4fc9da2CA41241f5e4263f'
+const notionalProxy_mainnet = '0x6e7058c91F85E0F6db4fc9da2CA41241f5e4263f'
+const notionalProxy_arbitrum = '0x1344A36A1B56144C3Bc62E7757377D288fDE0369'
 
-const getPrice = async (currencyId, nToken, underlyingToken) => {
+const getPrice = async (chainId = CHAIN_IDS.ETH_MAINNET, currencyId, nToken, underlyingToken) => {
+  let notionalProxy
+  const web3 = getWeb3(chainId)
+  if (chainId == CHAIN_IDS.ETH_MAINNET) notionalProxy = notionalProxy_mainnet
+  else notionalProxy = notionalProxy_arbitrum
   const underlyingPrice = await getTokenPrice(underlyingToken)
   const {
     methods: { getTotalSupply },
