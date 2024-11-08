2a041cb42fd83c382923b5cc825d501ab6f81ada
William Liu <william.liu@klinkfinance.com>
2024-11-08 18:45:53 +0000

    fix: update camelot api

--- src/lib/third-party/camelot.js
+++ src/lib/third-party/camelot.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 
 const getCamelotData = isV3 => {
   let uri = isV3
-    ? 'https://api.camelot.exchange/v2/liquidity-v3-data/'
-    : 'https://api.camelot.exchange/v2/liquidity-v2-pools-data/'
+    ? 'https://api.camelot.exchange/liquidity-v3-data/'
+    : 'https://api.camelot.exchange/liquidity-v2-pools-data/'
   let res = axios
     .get(uri)
     .then(response => get(response, 'data'))
