329882b33975e24a6f3e695b337b48ad8a58b75b
CryptJS13 <cryptjs13@protonmail.com>
2025-04-24 15:08:50 +0200

    Update drip

--- src/lib/web3/contracts/drip/methods.js
+++ src/lib/web3/contracts/drip/methods.js
@@ -1,7 +1,10 @@
 const { countFunctionCall } = require('../..')
 
 const getDrip = (idx, instance) => countFunctionCall(instance.methods.drips(idx).call())
+const getCurrentRate = (idx, instance) =>
+  countFunctionCall(instance.methods.getCurrentRate(idx).call())
 
 module.exports = {
   getDrip,
+  getCurrentRate,
 }
--- src/vaults/apys/implementations/drip.js
+++ src/vaults/apys/implementations/drip.js
@@ -1,33 +1,34 @@
 const BigNumber = require('bignumber.js')
 const { getWeb3 } = require('../../../lib/web3')
-const { drip } = require('../../../lib/web3/contracts')
+const { drip, plasmaVault } = require('../../../lib/web3/contracts')
 
 const getApy = async (vaultAddr, dripAddr, chain) => {
   const web3 = getWeb3(chain)
   const { methods: dripMethods, contract: dripContract } = drip
+  const { methods: vaultMethods, contract: vaultContract } = plasmaVault
 
   const dripInstance = new web3.eth.Contract(dripContract.abi, dripAddr)
   let foundDrip = false
   let idx = 0
-  let dripData
+  let dripData, currentRate
   while (!foundDrip) {
     try {
       dripData = await dripMethods.getDrip(idx, dripInstance)
+      currentRate = new BigNumber(await dripMethods.getCurrentRate(idx, dripInstance))
       if (dripData.vault == vaultAddr) {
         foundDrip = true
       }
     } catch (e) {
       foundDrip = true
-      dripData = null
     }
     idx++
   }
 
   let dripApy = new BigNumber(0)
-  if (dripData) {
-    if (dripData.mode == 1) {
-      dripApy = new BigNumber(dripData.perSecond).times(3600).times(24).times(365).div(1e16)
-    }
+  if (currentRate) {
+    const vaultInstance = new web3.eth.Contract(vaultContract.abi, vaultAddr)
+    const tvl = new BigNumber(await vaultMethods.getTotalAssets(vaultInstance))
+    dripApy = currentRate.times(3600).times(24).times(365).times(100).div(tvl).div(1e18)
   }
 
   return dripApy.toFixed()
