42df277fbbdbe3c03ed52ca0940b2838d1635f89
CryptJS13 <cryptjs13@protonmail.com>
2025-04-25 12:20:42 +0200

    Remove IPOR vaults from leaderboard

--- src/runtime/pollers.js
+++ src/runtime/pollers.js
@@ -822,6 +822,9 @@ const getLeaderboardData = async () => {
     '0x75071f2653fbc902ebaff908d4c68712a5d1c960',
     '0x156733b89ac5c704f3217fee2949a9d4a73764b5',
     '0xf1181a71cc331958ae2ca2aad0784acfc436cb93',
+    '0x31a421271414641cb5063b71594b642d2666db6b',
+    '0x7872893e528Fe2c0829e405960db5B742112aa97',
+    '0x0d877dc7c8fa3ad980dfdb18b48ec9f8768359c4',
   ]
   const CHAIN_NAMES = {
     1: 'eth',
